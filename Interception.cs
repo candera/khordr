f1e0da3c96696f0e9955088b5451f4ff7d44d6ae
Craig Andera <candera@wangdera.com>
2012-03-02 10:56:16 -0500

    Refactored code, added tests

---
+++ Interception.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace kchordr
+{
+    public class Interception
+    {
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate int Predicate(int device);
+
+        [Flags]
+        public enum KeyState
+        {
+            Down = 0x00,
+            Up = 0x01,
+            E0 = 0x02,
+            E1 = 0x04,
+            TermsrvSetLED = 0x08,
+            TermsrvShadow = 0x10,
+            TermsrvVKPacket = 0x20
+        }
+
+        [Flags]
+        public enum Filter : ushort
+        {
+            None = 0x0000,
+            All = 0xFFFF,
+            KeyDown = KeyState.Up,
+            KeyUp = KeyState.Up << 1,
+            KeyE0 = KeyState.E0 << 1,
+            KeyE1 = KeyState.E1 << 1,
+            KeyTermsrvSetLED = KeyState.TermsrvSetLED << 1,
+            KeyTermsrvShadow = KeyState.TermsrvShadow << 1,
+            KeyTermsrvVKPacket = KeyState.TermsrvVKPacket << 1
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MouseStroke
+        {
+            public ushort state;
+            public ushort flags;
+            public short rolling;
+            public int x;
+            public int y;
+            public uint information;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct KeyStroke
+        {
+            public ushort code;
+            public ushort state;
+            public uint information;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct Stroke
+        {
+            [FieldOffset(0)]
+            public MouseStroke mouse;
+
+            [FieldOffset(0)]
+            public KeyStroke key;
+        }
+
+        [DllImport("interception.dll", EntryPoint = "interception_create_context", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr CreateContext();
+
+        [DllImport("interception.dll", EntryPoint = "interception_destroy_context", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void DestroyContext(IntPtr context);
+
+        [DllImport("interception.dll", EntryPoint = "interception_set_filter", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void SetFilter(IntPtr context, Predicate predicate, Filter filter);
+
+        [DllImport("interception.dll", EntryPoint = "interception_receive", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int Receive(IntPtr context, int device, ref Stroke stroke, uint nstroke);
+
+        [DllImport("interception.dll", EntryPoint = "interception_send", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int Send(IntPtr context, int device, ref Stroke stroke, uint nstroke);
+
+        [DllImport("interception.dll", EntryPoint = "interception_is_keyboard", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int IsKeyboard(int device);
+
+        [DllImport("interception.dll", EntryPoint = "interception_wait", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int Wait(IntPtr context);
+    }
+}
---
+++ KeyComputer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace kchordr
+{
+    public class KeyComputer
+    {
+        private readonly Queue<Interception.Stroke> _transmissions = new Queue<Interception.Stroke>();
+
+        public Queue<Interception.Stroke> Transmissions
+        {
+            get { return _transmissions; }
+        }
+
+        public void Receive(Interception.Stroke stroke)
+        {
+            if (stroke.key.code == ScanCode.J && stroke.key.state == (ushort) Interception.KeyState.Up)
+            {
+
+                _transmissions.Enqueue(
+            }
+            _transmissions.Enqueue(stroke);
+        }
+    }
+}

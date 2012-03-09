bcce318a9bb64ee406c226da2eb4d1c0a3685044
Craig Andera <candera@wangdera.com>
2012-03-09 14:39:34 -0500

    Add Interception to a local Maven repo.

--- project.clj
+++ project.clj
@@ -1,3 +1,10 @@
 (defproject kchordr "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 ;[net.java.dev.jna/jna "3.4.0"]
+                 [kchordr/interception "1.0.0"]]
+  :source-paths ["src/clj"]
+  ;; Use one of the two following, depending on whether you want
+  ;; source or jar dependency
+  ;; :java-source-path "src/java"
+  :repositories {"local" "file:repo"})

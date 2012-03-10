cbd5c8b4d6878f28a2a73392ccfc130dbf74d9c0
Craig Andera <candera@wangdera.com>
2012-03-10 07:00:35 -0500

    [WIP] Play around with how to reference JNA code.

--- project.clj
+++ project.clj
@@ -2,9 +2,12 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  ;[net.java.dev.jna/jna "3.4.0"]
-                 [kchordr/interception "1.0.0"]]
+                 ;[kchordr/interception "1.0.0"]
+                 ]
   :source-paths ["src/clj"]
   ;; Use one of the two following, depending on whether you want
   ;; source or jar dependency
-  ;; :java-source-path "src/java"
-  :repositories {"local" "file:repo"})
+  :java-source-path "src/java"
+  ;; :repositories {"local" "file:repo"}
+  ;; :jvm-opts "-Djna.library.path=ext"
+  )

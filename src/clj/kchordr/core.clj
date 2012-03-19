84703d3a1e8a805fede9928747be7aa210c034aa
Craig Andera <candera@wangdera.com>
2012-03-19 14:11:38 -0400

    [WIP] Continue work towards modifier aliasing.

--- src/clj/kchordr/core.clj
+++ src/clj/kchordr/core.clj
@@ -29,49 +29,78 @@
   state-transitions
   {[:ground :normal :up] :ground
    [:ground :normal :dn] :ground
-   [:ground :modifier-alias :dn] :mod-pending
+   [:ground :modifier-alias :dn] :mod-undecided
    [:ground :modifier-alias :up] :error})
 
+(defn modifier-alias?
+  "Given a key class, return true if it is a modifier alias"
+  [cls]
+  (and (coll? cls) (= (first cls) :modifier-alias)))
+
+(defn undecided?
+  "Given a key and its state, return true if it is undecided."
+  [[_ s]]
+  (= :undecided s))
+
+
 ;; State looks something like
-;; {:to-send [] :keystate {:j :pending, :k :lcontrol}}
+;; {:to-send [] :keystate {:j :undecided, :k :lcontrol}}
 
-(defn pending-modifier?
-  "Return true when the key state contains a pending modifier key."
-  [state]
-  (some #(= :pending %) (vals (:keystate state))))
+(defn undecided-modifier?
+  "Return true when the keystate contains a undecided modifier key."
+  [keystate]
+  (some undecided? keystate))
 
-(defn pending-modifier-downs
-  "Return a seq of keypresses for the pending modifiers."
-  [state]
-  (throw (NotImplmentedException.)))
+(defn regular-key?
+  "Return true if key is not an aliased key."
+  [key]
+  (not (get key-classes key nil)))
 
-;; {:j :pending :k :lcontrol} => {:j :lshift :k :lcontrol}
-(defn decide-pending-modifiers
-  "Return a sequence of key events for pending modifiers, deciding
-  them to be their aliased equivalents."
-  [state]
-  (into {} (map (fn [[k v]] (if (= :pending v)
-                              [k (second (k (key-classes)))]
-                              [k v])))))
+(defn regular-keydown?
+  "Return true if the key event is a key down event where the key is
+  not aliased."
+  [key direction]
+  (and (regular-key? key) (= direction :dn)))
 
+(defn undecided-modifier-downs
+  "Return a seq of keypresses for the undecided modifiers."
+  [keystate]
+  (->> keystate
+       (filter undecided?)
+       (map first)
+       (mapcat (fn [k] [k :dn]))))
+
+;; {:j :undecided :k :lcontrol} => {:j :lshift :k :lcontrol}
+(defn decide-modifiers
+  "Return a sequence of key events for undecided modifiers, deciding
+  them to be their aliased equivalents.
+
+  {:j :undecided :k :undecided :l :lalt} =>
+  [:rshift :dn :rcontrol :dn]"
+  [keystate]
+  (into {} (map (fn [ks] (if (undecided? ks)
+                           [(first ks) (second (get key-classes (second k)))]
+                           ks))
+                keystate)))
 
 (defn process
   "Given the current key state and a key event, return an updated key
   state."
   [state key direction]
-  (let [cls (get key-classes key :normal)]
+  (let [cls (get key-classes key :normal)
+        keystate (:keystate state)]
     (cond
-     (and (= cls :modifier-alias) (= :dn direction))
-     (update-in state [:keystate key] (constantly :pending))
+     (and (modifier-alias? cls) (= :dn direction))
+     (update-in state [:keystate key] (constantly :undecided))
 
-     (and (pending-modifier? state)
+     (and (undecided-modifier? keystate)
           (regular-keydown? key direction))
      (assoc state
        :to-send (concat (:to-send state)
-                        (pending-modifier-downs state)
+                        (undecided-modifier-downs keystate)
                         [key :dn])
-       :keystate (apply assoc (:keystate state)
-                   (decide-pending-modifiers state)))
+       :keystate (apply assoc keystate
+                   (decide-modifiers keystate)))
 
      :else
      (update-in state [:to-send] #(conj % [key direction])))))

556de87c79ac989dcccdb1bd65b376e7c830abaa
Craig Andera <candera@wangdera.com>
2012-03-23 16:23:05 -0400

    [WIP] Refactor tests.

--- test/kchordr/test/core.clj
+++ test/kchordr/test/core.clj
@@ -5,29 +5,29 @@
 (defn- sent
   "Given a sequence of key events, return the sequence of keys that
   will actually be sent."
-  [pressed]
-  (loop [state (key-state)
-         presses pressed]
+  [presses]
+  (loop [state (state)
+         press presses]
     (if (seq presses)
-      (recur (process state (first presses) (second presses))
+      (recur (process state press)
              (rest (rest presses)))
       (to-send state))))
 
 (deftest key-tests
-  (are [pressed expected-sent] (= (partition 2 expected-sent) (sent pressed))
+  (are [pressed anticipated] (= anticipated (sent pressed))
        ;; Single non-modifier key press
-       [:b :dn]
-       [:b :dn]
+       [[:b :dn]]
+       [[:b :dn]]
 
        ;; Single non-modified key press and release
-       [:b :dn :b :up]
-       [:b :dn :b :up]
+       [[:b :dn] [:b :up]]
+       [[:b :dn] [:b :up]]
 
        ;; Modified key press only
-       [:j :dn]
+       [[:j :dn]]
        []
 
        ;; Modified shifted key
-       [:j :dn :x :dn]
-       [:rshift :dn :x :dn]))
+       [[:j :dn] [:x :dn]]
+       [[:rshift :dn] [:x :dn]]))
 

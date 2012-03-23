f150fc0886e8430d64813493b9f72c23d9395672
Craig Andera <candera@wangdera.com>
2012-03-23 14:28:57 -0400

    Updated tests to use are rather than weird combo of is and macros

--- test/kchordr/test/core.clj
+++ test/kchordr/test/core.clj
@@ -2,46 +2,32 @@
   (:use [kchordr.core])
   (:use [clojure.test]))
 
-(defn- key-test
-  "Given a sequence of key events, check that the result of processing
-  them is correct."
-  [pressed expected description]
-  (is (= (partition 2 expected)
-         (loop [state (key-state)
-                presses pressed]
-           (if (seq presses)
-             (recur (process state (first presses) (second presses))
-                    (rest (rest presses)))
-             (to-send state))))
-      description))
+(defn- sent
+  "Given a sequence of key events, return the sequence of keys that
+  will actually be sent."
+  [pressed]
+  (loop [state (key-state)
+         presses pressed]
+    (if (seq presses)
+      (recur (process state (first presses) (second presses))
+             (rest (rest presses)))
+      (to-send state))))
 
-(defn- test-form [description pressed expected]
-  (let [name (->> pressed
-                  (map name)
-                  (interpose "-")
-                  (apply str)
-                  symbol)]
-   `(deftest ~name
-      (key-test ~pressed ~expected ~description))))
+(deftest key-tests
+  (are [pressed expected-sent] (= (partition 2 expected-sent) (sent pressed))
+       ;; Single non-modifier key press
+       [:b :dn]
+       [:b :dn]
 
-(defmacro defkeytests [& body]
-  (concat '(do)
-          (map #(apply test-form %) (partition 3 body))))
+       ;; Single non-modified key press and release
+       [:b :dn :b :up]
+       [:b :dn :b :up]
 
-(defkeytests
-  "Single non-modified key down"
-  [:b :dn]
-  [:b :dn]
+       ;; Modified key press only
+       [:j :dn]
+       []
 
-  "Single non-modified key press and release"
-  [:b :dn :b :up]
-  [:b :dn :b :up]
-
-  "Modified key press only"
-  [:j :dn]
-  []
-
-  "Modified shifted key"
-  [:j :dn :x :dn]
-  [:lshift :dn :x :dn])
+       ;; Modified shifted key
+       [:j :dn :x :dn]
+       [:rshift :dn :x :dn]))
 

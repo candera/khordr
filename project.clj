d9b8bc6414b21a79851d3965249939a4da8355ab
Craig Andera <candera@wangdera.com>
2012-05-27 17:11:50 -0400

    Upgrade to lein2

--- project.clj
+++ project.clj
@@ -6,11 +6,11 @@
                  [com.nativelibs4java/jnaerator-runtime "0.9.10-SNAPSHOT"]
                  ;;[kchordr/interception "1.0.0"]
                  ]
-  :dev-dependencies [[lein-swank "1.4.4"]]
-  :source-path "src/clj"
+  :plugins [[lein-swank "1.4.4"]]
+  :source-paths ["src/clj"]
   ;; Use one of the two following, depending on whether you want
   ;; source or jar dependency
-  :java-source-path "src/java"
+  :java-source-paths ["src/java"]
   :repositories {"local" "file:repo"
                  "sonatype" "http://oss.sonatype.org/content/repositories/releases"
                  "nativelibs4java-repo" "http://nativelibs4java.sourceforge.net/maven"}

cc9c2ebe6ed9a7a471e9ff78e940776adc481262
Craig Andera <candera@wangdera.com>
2012-09-23 17:42:59 -0500

    Get rid of DefaultKeyHandler

--- src/clj/khordr/handler.clj
+++ src/clj/khordr/handler.clj
@@ -12,22 +12,7 @@
 
   :effects - a seq of commands legal for consumption by `engine`."))
 
-;; The default implementation: pass through key events unchanged.
-;; yourself when self-key goes up.
-(defrecord DefaultKeyHandler [self-key]
-  KeyHandler
-  (process [this _ keyevent]
-    (let [{:keys [key direction]} keyevent]
-      {:handler (when-not (and (= direction :up)
-                               (= key (:self-key this)))
-                  this)
-       :effects [{:effect :key :event keyevent}]})))
-
-;; And if we're not tracking any keys (as can happen if the first
-;; event is a key up), just pass through the events unchanged. The
-;; reason we want both this and DefaultKeyHandler is that
-;; DefaultKeyHandler will prevent any other key down event from
-;; activating a handler.
+;; In the absence of any other key handler, just pass events through
 (extend-protocol KeyHandler
   nil
   (process [_ _ keyevent]

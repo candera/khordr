f3412f2a2c9f668fd913dbfc6dec7b37cb0cd4ba
Craig Andera <candera@wangdera.com>
2012-10-08 21:27:21 -0400

    Fix bug where special action handler was not suppressing the trigger key.

--- src/clj/khordr/handler/special_action.clj
+++ src/clj/khordr/handler/special_action.clj
@@ -4,30 +4,30 @@
   (:require [khordr.handler :as h])
   (:import [khordr.effect Key Quit CycleLog]))
 
-(defrecord InitializedHandler [trigger]
+(defrecord InitializedHandler [trigger acting]
   h/KeyHandler
   (process [this state keyevent]
     (let [{:keys [key direction]} keyevent]
       (cond
        (and (= key trigger) (= direction :up))
        {:handler nil
-        :effects [(Key. (assoc keyevent :key trigger :direction :dn))
-                  (Key. (assoc keyevent :key trigger :direction :up))]}
+        :effects (when-not acting
+                   [(Key. (assoc keyevent :key trigger :direction :dn))
+                    (Key. (assoc keyevent :key trigger :direction :up))])}
 
        (and (= key :q) (= direction :dn))
-       {:handler this
+       {:handler (InitializedHandler. trigger true)
         :effects [(Quit.)]}
 
        (and (= key :l) (= direction :dn))
-       {:handler this
+       {:handler (InitializedHandler. trigger true)
         :effects [(CycleLog.)]}
 
        :else
-       {:handler this}))))
+       {:handler (InitializedHandler. trigger true)}))))
 
 ;; The first key event we get is the thing that triggered us
 (defrecord Handler []
   h/KeyHandler
   (process [this state keyevent]
-    {:handler (InitializedHandler. (:key keyevent))}))
-
+    {:handler (InitializedHandler. (:key keyevent) false)}))
